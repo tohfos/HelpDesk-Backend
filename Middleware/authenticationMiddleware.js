3939b8b4fe29fe02269c4ac8cdb387efb0cc7cd9
tohfos <119699153+tohfos@users.noreply.github.com>
2023-11-22 18:56:35 +0200

    initial

---
+++ Middleware/authenticationMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const secretKey = "s1234rf,.lp";
+
+module.exports = function authenticationMiddleware(req, res, next) {
+  const cookie = req.cookies;
+  
+  // console.log(req.headers);
+
+  if (!cookie) {
+    return res.status(401).json({ message: "No Cookie provided" });
+  }
+  const token = cookie.token;
+  if (!token) {
+    return res.status(405).json({ message: "No token provided" });
+  }
+
+  jwt.verify(token, secretKey, (error, decoded) => {
+    if (error) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    // Attach the decoded user ID to the request object for further use
+    // console.log(decoded.user)
+    
+    req.user = decoded.user;
+    next();
+  });
+};
---
+++ Middleware/authorizationMiddleware.js
@@ -0,0 +1,10 @@
+module.exports= function authorizationMiddleware(roles) {
+    return (req, res, next) => {
+      console.log('req:',req)
+      const userRole = req.user.role;
+      if (!roles.includes(userRole))
+        return res.status(403).json("unauthorized access");
+      // console.log('authormid')
+      next();
+    };
+  }
