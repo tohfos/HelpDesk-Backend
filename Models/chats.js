0f3eac2ce416161c24b472dfc32a3e15d7e73661
Abdullah abooof <abdullah.abooof@gmail.com>
2023-11-24 15:19:31 +0200

    chat schema added

---
+++ Models/chats.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const userModel = require("./usersModel.j")
+
+const chatSchema = new mongoose.Schema({
+
+    userId: { 
+        type: type.Schema.Types.ObjectID,
+        ref: "userModel"
+    },
+    agentId: {
+        type: type.Schema.Types.ObjectID,
+        ref: "userModel" 
+        },
+    timestamp: {
+        type: Date,
+        default: Date.now 
+        },
+    message:{
+        type: String,
+        minLength:1
+    } 
+
+
+
+
+})
