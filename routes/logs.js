7c21d1e379a49b486301e7e73275b9f2f4f501d3
Mahmouddd21 <mahmoudosama11.msed@gmail.com>
2023-11-26 19:33:17 +0200

    ay 7aga

--- routes/logs.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-// const LogsModel = require("../models/logsModel");
-const logController = require("../controller/logController");
-
-router.get("/", logController.getAllLogs);
-
-module.exports = router;
