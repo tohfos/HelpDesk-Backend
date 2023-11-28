5e82ff941aa795241164c9a6f86a6c794533d2e6
tohfos <119699153+tohfos@users.noreply.github.com>
2023-11-28 18:33:54 +0200

    get profile done

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -6,6 +6,7 @@ const authorizationJWT = require("../Middleware/authorizeJWT");
 
 router.post('/create', authorizationJWT(['User']), userController.createTicket);
 router.get('/get', authorizationJWT(['User']), userController.getTicket);
+router.get('/profile',authorizationJWT(['User']), userController.getProfile);
 
 
 
--- controller/userController.js
+++ controller/userController.js
@@ -45,6 +45,19 @@ const userController = {
         }
       },
 
+      getProfile: async (req, res) => {
+
+        try{
+          
+          const user = await usersModel.findById(req.userId);
+          if(!user){return res.status(404).json({message:"No user found"})}
+          return res.status(200).json(user.profile);
+        } catch(error){
+          return res.status(500).json({ message: error.message });
+        }
+
+      },
+
 
 }
     //helper method to assigne agents based on category
