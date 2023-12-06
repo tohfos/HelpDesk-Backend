7ecea235332da1694e02e9611aff08d9c1896218
omar2 <omar1772003@gmail.com>
2023-12-06 02:08:48 +0200

    ,,

--- backup.js
+++ backup.js
@@ -1,23 +1,14 @@
 const { spawn } = require('child_process');
 const path = require('path');
+const cron = require('node-cron');
 
-/* 
-Basic mongo dump and restore commands, they contain more options you can have a look at man page for both of them.
-1. mongodump --db=rbac_tutorial --archive=./rbac.gzip --gzip
-2. mongorestore --db=rbac_tutorial --archive=./rbac.gzip --gzip
 
-Using mongodump - without any args:
-  will dump each and every db into a folder called "dump" in the directory from where it was executed.
-Using mongorestore - without any args:
-  will try to restore every database from "dump" folder in current directory, if "dump" folder does not exist then it will simply fail.
-*/
 
 const DB_NAME = 'HelpDesk';
 const ARCHIVE_PATH = path.join(__dirname, `${DB_NAME}.gzip`);
 
-// 1. Cron expression for every 5 seconds - */5 * * * * *
-// 2. Cron expression for every night at 00:00 hours (0 0 * * * )
-// Note: 2nd expression only contains 5 fields, since seconds is not necessary
+
+cron.schedule('0 0 0 * * *', () => backupMongoDB());
 
 
 function backupMongoDB() {
