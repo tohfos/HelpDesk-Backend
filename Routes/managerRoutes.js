0539c0e368e47b8d948619a932f0e54410989058
Akmas14 <abdullah_kmas@outlook.com>
2023-12-13 01:02:01 +0200

    analytics done bas without testing

--- Routes/managerRoutes.js
+++ Routes/managerRoutes.js
@@ -4,4 +4,7 @@ const ManagerController = require("../controller/managerController");
 const authorizationJWT = require("../Middleware/authorizeJWT");
 
 router.post('/generateReport/:ticketId',authorizationJWT(['Manager']),ManagerController.generateReport);
-router.get('/getReport/:id',authorizationJWT(['Manager']),ManagerController.getReportByID)
+router.get('/getReport/:id',authorizationJWT(['Manager']),ManagerController.getReportByID);
+router.post('/generateAnalytics1/:id',authorizationJWT(['Manager']),ManagerController.generateAnalyticsForAgent);
+router.post('/generateAnalytics2/:SubCategory',authorizationJWT(['Manager']),ManagerController.generateAnalyticsForSubCategory);
+router.post('/generateAnalytics3/:Category',authorizationJWT(['Manager']),ManagerController.generateAnalyticsForCategory);
